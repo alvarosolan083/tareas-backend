dcc1ad2a2a0647512cd67c011633f5831dad726b
Álvaro Solano <alvarosolano569@gmail.com>
2025-05-14 21:35:05 -0400

    fix(cors): permitir origen desde Vercel y localhost

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: [
+      'http://localhost:5173', // Desarrollo local
+      'https://tareas-frontend-dusky.vercel.app' // Producción Vercel
+    ],
     credentials: true,
   });
 
   await app.listen(3000);
 }
 bootstrap();
-
